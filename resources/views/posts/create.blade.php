04c22d289684ae60c74d38b7a20e97d8ce703c68
fballa <frankball4@gmail.com>
2024-05-02 15:37:08 -0600

    primera version del blog

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+  <h1>Nuevo BLOG</h1>
+  <section class="mt-3">
+    <form method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+      @csrf
+      
+      @if ($errors->any())
+        <div class="alert alert-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
+      <div class="card p-3">
+        <label for="floatingInput">Titulo</label>
+        <input class="form-control" type="text" name="title">
+        <label for="floatingTextArea">Descripcion</label>
+        <textarea class="form-control" name="description" id="floatingTextarea" cols="30" rows="10"></textarea>
+        <label for="formFile" class="form-label">Agregar Imagen</label>
+        <img src="" alt="" class="img-blog">
+        <input class="form-control" type="file" name="image">
+      </div>
+      <button class="btn btn-success m-3">GUARDAR</button>
+      <a class="btn btn-primary float-end mt-3" href="{{ route('posts.index') }}" role="button">Cancelar</a>
+    </form>
+  </section>
+    
+</div>
+@endsection
